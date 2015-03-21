051cfae731b49cafc41be322c81fee3184e538a1
Jiuchu <le9227034470@yahoo.fr>
2015-03-21 20:01:42 +0100

    Update exo2.ml

--- tp2/src/exo2.ml
+++ tp2/src/exo2.ml
@@ -5,3 +5,9 @@
     | [] -> []
     | [e] -> e
     | t::q -> append (append t [' ']) (to_single_list q);;
+  let print l =
+    let l = to_single_list l in
+      let rec display l = match l with
+	    |[] -> print_newline ()
+	    |t::q -> print_char t;
+	         display q in display q ;;
